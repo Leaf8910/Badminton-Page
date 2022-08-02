d097b2fba6f8da19d889b0b1bf81fff633aa8ace
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-08-02 21:15:49 +0800

    Added opening times and mapbox

--- app2.js
+++ app2.js
@@ -5,8 +5,125 @@ let nextBtn = document.querySelector('.next-btn');
 let paginationArea = document.querySelector('.pagination-area');
 let currentImageCount = 2;
 let sliderController;
-let createPaginationSpans;
+let createPaginationSpans; 
+let mapbox_API = `https://api.mapbox.com/geocoding/v5/mapbox.places/bandar.json?country=bn&proximity=ip&types=place%2Cpostcode%2Caddress&access_token={mapboxToken}`;
+let mapBox =  document.querySelector(".mapBox");
+let peakHours = document.querySelector(".peak-hours");
 
+// mapboxg1.accessToken = "pk.eyJ1IjoibGVhZjg5MSIsImEiOiJjbDZiYmIxMWsxYXBjM2RwM3g5Y2R5ZTVzIn0._3mYOHsO_qd2XL7DfwbNaA";
+// const mapB = new mapboxg1.Map({
+//     container: 'map', 
+//     style: 'mapbox://styles/leaf891/cl6bldccm000514qnwsj5rdtb',
+//     center: [4.9031, 114.9398], 
+//     zoom: 7 
+// })
+
+
+
+// const places = {
+//     'type': 'FeatureCollection',
+//     'features': [
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': "Ford's Theater",
+//     'icon': 'theatre-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [4.937683543922975, 114.92901856839401]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': 'The Gaslight',
+//     'icon': 'theatre-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.003168, 38.894651]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': "Horrible Harry's",
+//     'icon': 'bar-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.090372, 38.881189]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': 'Bike Party',
+//     'icon': 'bicycle-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.052477, 38.943951]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': 'Rockabilly Rockstars',
+//     'icon': 'music-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.031706, 38.914581]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': 'District Drum Tribe',
+//     'icon': 'music-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.020945, 38.878241]
+//     }
+//     },
+//     {
+//     'type': 'Feature',
+//     'properties': {
+//     'description': 'Motown Memories',
+//     'icon': 'music-15'
+//     },
+//     'geometry': {
+//     'type': 'Point',
+//     'coordinates': [-77.007481, 38.876516]
+//     }
+//     }
+//     ]
+// };
+
+// map.on('load', () => {
+//     // Add a GeoJSON source containing place coordinates and information.
+//     map.addSource('places', {
+//     'type': 'geojson',
+//     'data': places
+//     });
+     
+//     map.addLayer({
+//     'id': 'poi-labels',
+//     'type': 'symbol',
+//     'source': 'places',
+//     'layout': {
+//     'text-field': ['get', 'description'],
+//     'text-variable-anchor': ['top', 'bottom', 'left', 'right'],
+//     'text-radial-offset': 0.5,
+//     'text-justify': 'auto',
+//     'icon-image': ['get', 'icon']
+//     }
+// })});
+
+// Wrapper Fucntions
 // Every Click On Buttons
 previousBtn.addEventListener('click', previousImage);
 nextBtn.addEventListener('click', nextImage);
@@ -86,3 +203,46 @@ setInterval(() => {
   };
 }, 3000);
 
+
+
+let peakTime = document.querySelector(".currently-peak");
+let dewanTime = document.querySelector(".currently-dewan");
+let msTime = document.querySelector(".currently-mscourt");
+var current = new Date();
+var hour = current.getHours();
+var day = current.getDay();
+
+console.log(hour);
+console.log("current hour " + current.getHours());
+console.log("Current day " + current.getDay()); // 0 is Sunday
+
+if (hour > 6 && hour < 22) {
+  peakTime.innerHTML = "OPEN";
+  peakTime.style.color = "green";
+  console.log("OPEN")
+} else if (hour < 6 && hour > 22){
+  peakTime.innerHTML = "CLOSED";
+  peakTime.style.color = "red";
+  console.log("CLOSED")
+} else {
+  peakTime.innerHTML = "???"
+  console.log("WHAT")
+}
+
+if (hour > 6 && hour < 22) {
+  dewanTime.innerHTML = "OPEN";
+  msTime.innerHTML = "OPEN";
+  dewanTime.style.color = "green";
+  msTime.style.color = "green";
+  console.log("OPEN")
+} else if (hour < 6 && hour > 22){
+  dewanTime.innerHTML = "CLOSED";
+  msTime.innerHTML = "CLOSED";
+  dewanTime.style.color = "red";
+  msTime.style.color = "red";
+  console.log("CLOSED")
+} else {
+  dewanTime.innerHTML = "???"
+  msTime.innerHTML = "???"
+  console.log("WHAT")
+}
