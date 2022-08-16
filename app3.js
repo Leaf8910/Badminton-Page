db5c62ddfebd2223158e11a8d70d7b97905a2064
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-08-17 06:24:48 +0800

    Fixed child divs

--- app3.js
+++ app3.js
@@ -2,7 +2,7 @@ import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
 
-let newsCard1 = document.querySelector('.news-cards1');
+
 
 async function getHeadlines() {
     try {
@@ -11,40 +11,23 @@ async function getHeadlines() {
         const body = await response.text();
         const $ = cheerio.load(body);
         
-        const items = [];
-        $('#tdi_85 > div:nth-child(1) > div > div.td-module-meta-info').map((i, el) => {
-
+        const items = []; //-n + 4 for the first five nth children
+        $('#tdi_85 > div:nth-child(-n + 4) > div > div.td-module-meta-info').map((i, el) => {
             const rank = $(el).find('a').text();
             console.log(rank.slice(6,70));
             const date = $(el).find('time').text();
             console.log(date);
-            newsCard1.innerHTML = rank.slice(6, 70);
-        });
-        
-        const items2 = [];
-        $('#tdi_85 > div:nth-child(2) > div > div.td-module-meta-info').map((i, el) => {
-
-            const rank = $(el).find('div.td-module-meta-info > h3 > a').text();
-            console.log(rank);
-            const date = $(el).find('time').text();
-            console.log(date);
-
-        }); 
-
-        const items3 = [];
-        $('#tdi_85 > div:nth-child(3) > div > div.td-module-meta-info').map((i, el) => {
-
-            const rank = $(el).find('a').text();
-            console.log(rank.slice(6,70));
-            const date = $(el).find('time').text();
-            console.log(date);
+            const href = $(el).find('h3 > a').attr('href');
+            console.log(href);
+            //Cant get background img cause of url inline
+            // const img = $(el).find(' a > span').attr('style > background-image');
+            // console.log(img)
             
-
         });
 
         
     } catch (error) {
-        console.log(Err)
+        console.log(error)
     }
 }
 
